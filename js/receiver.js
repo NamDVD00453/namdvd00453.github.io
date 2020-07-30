8a2848e19610f798c44463b52b7c8949f2153829
NamDVD00453 <namdvd00453@fpt.edu.vn>
2020-07-30 15:14:13 +0700

    Add Demo

---
+++ js/receiver.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const localVideo = document.getElementById('localVideo');
+const remoteVideo = document.getElementById('remoteVideo');
+const senderId = document.getElementById('senderId');
+
+const callButton = document.getElementById('callButton');
+
+callButton.addEventListener('click', function () {
+    callButton.disabled = true;
+
+    var peer = new Peer(senderId.value, {
+        host: 'localhost',
+        port: 9000,
+        path: '/myapp'
+    });
+    console.log(senderId.value)
+    var getUserMedia = navigator.getUserMedia;
+    peer.on('call', function(call) {
+        getUserMedia({video: true, audio: false}, function(stream) {
+            localVideo.srcObject = stream;
+            call.answer(stream);
+
+            call.on('stream', function(remoteStream) {
+                remoteVideo.srcObject = remoteStream;
+            });
+        }, function(err) {
+            console.log('Failed to get local stream' ,err);
+            alert('Failed to get local stream');
+        });
+    });
+})
---
+++ js/sender.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const localVideo = document.getElementById('localVideo');
+const remoteVideo = document.getElementById('remoteVideo');
+const PsenderID = document.getElementById('PsenderID');
+
+const startButton = document.getElementById('startButton');
+
+startButton.addEventListener('click', function () {
+  startButton.disabled = true;
+  var senderId = makeid(10);
+    var peer = new Peer(senderId, {
+        host: 'localhost',
+        port: 9000,
+        path: '/myapp'
+    });
+
+    console.log('Sender ID: ' + senderId);
+
+
+    var receiverId = makeid(10);
+    PsenderID.textContent = 'Call ID: ' + receiverId;
+    PsenderID.hidden = false;
+
+    var getUserMedia = navigator.getUserMedia;
+    getUserMedia({video: true, audio: false}, function(stream) {
+        localVideo.srcObject = stream;
+        var call = peer.call(receiverId, stream);
+
+        call.on('stream', function(remoteStream) {
+            remoteVideo.srcObject = remoteStream;
+        });
+    }, function(err) {
+        console.log('Failed to get local stream' ,err);
+        alert('Failed to get local stream');
+    });
+})
+
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
